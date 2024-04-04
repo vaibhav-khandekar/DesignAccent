1181cc44d7c108776d6643e9657a35fc1eb8f576
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-04-04 18:24:29 +0530

    Update WebForm1.aspx.cs

--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -28,6 +28,7 @@ namespace WebApplication2
                 string str = "insert into Employee values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text + "')";
                 SqlCommand cmd = new SqlCommand(str, con);
                 cmd.ExecuteNonQuery();
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('data inserted is successfully')", true);
                 con.Close();
             }
         }
@@ -37,19 +38,45 @@ namespace WebApplication2
         protected void Button2_Click(object sender, EventArgs e)
         {
             con.Open();
-            string str = "update Employee set EmpName='" + TextBox2.Text + "', EmpSalary='" + TextBox3.Text + "',EmpAge='"+TextBox4.Text + "'where EmpID='" + TextBox1.Text + "'";
-            SqlCommand cmd = new SqlCommand(str, con);
-            cmd.ExecuteNonQuery();
-            con.Close();
+            SqlCommand cmdToCheck = new SqlCommand("select count(EmpID) from Employee where EmpID='" + TextBox1.Text + "'", con);
+            //SqlDataAdapter sd = new SqlDataAdapter(cmdToCheck);
+            //DataTable dt = new DataTable();
+
+           int id = Convert.ToInt32(cmdToCheck.ExecuteScalar());
+
+            if (id > 0)
+            {
+                string str = "update Employee set EmpName='" + TextBox2.Text + "', EmpSalary='" + TextBox3.Text + "',EmpAge='" + TextBox4.Text + "'where EmpID='" + TextBox1.Text + "'";
+                SqlCommand cmd = new SqlCommand(str, con);
+                cmd.ExecuteNonQuery();
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('data updated is successfully')", true);
+                con.Close();
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('row is not available')", true);
+            }
         }
 
         
         protected void Button3_Click(object sender, EventArgs e)
         {
-                con.Open();
-                SqlCommand cmd = new SqlCommand("delete from Employee where EmpID='" + TextBox1.Text + "'", con);
-                cmd.ExecuteNonQuery();
-                con.Close();
+            con.Open();
+            SqlCommand cmdToCheck = new SqlCommand("select count(EmpID) from Employee where EmpID='" + TextBox1.Text + "'", con);
+            //SqlDataAdapter sd = new SqlDataAdapter(cmdToCheck);
+            //DataTable dt = new DataTable();
+            int id = Convert.ToInt32(cmdToCheck.ExecuteScalar());
+            if (id > 0)
+                {
+                    SqlCommand cmd = new SqlCommand("delete from Employee where EmpID='" + TextBox1.Text + "'", con);
+                    cmd.ExecuteNonQuery();
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('data deleted is successfully')", true);
+                    con.Close();
+                }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('row is not availableeeeee')", true);
+            }
         }
 
         protected void Button4_Click(object sender, EventArgs e)

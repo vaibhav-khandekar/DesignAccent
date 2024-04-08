9195e700e72eb7fa9f144157a8d72c69f899acf5
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-04-08 17:31:44 +0530

    Update validation.js

--- upliftjeraifitness.com/validation.js
+++ upliftjeraifitness.com/validation.js
@@ -22,6 +22,13 @@ function validate()
         return false;
     }
 
+    var regExp = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}/;
+    var phone_reg = phone.match(regExp);
+    if (!(phone)) {
+        alert('please enter valid phone number');
+        return false;
+    }
+
     // state
     var state = document.getElementById("selectState");
     var state_val = state.options[state.selectedIndex].value;
@@ -48,9 +55,10 @@ function validate()
 
     // bodybuilding / powerlifting
 
-    var tabName = document.getElementById("TabBodybuilding").getAttribute("rel");
-    if (tabName === 'TabBodybuilding') {
-        // bodybuilding tab
+    var tabName;
+
+    // bodybuilding tab
+    //if (tabName === 'TabBodybuilding') {
         // past achievements and competitions won
         var build_won = document.getElementsByName('rdoAchievementsBodyBuilding');
 
@@ -67,10 +75,36 @@ function validate()
             return false;
         }
 
-    }
-    else if (tabName === 'TabPowerlifting') {
+        // date of completion
+        var dateInput = document.getElementById("txtdateOfBodyBuilding").value;
+        if (dateInput === "") {
+            alert("Please select a date");
+        }
 
-        // powerlifting tab
+        // photo link 1
+        let pl_bb_1 = document.forms["Jerai_form"]["txtPhotoLink1BodyBuilding"].value;
+        if (pl_bb_1 == "") {
+            alert("please enter photo link 1");
+            return false;
+        }
+
+        // photo link 2
+        let pl_bb_2 = document.forms["Jerai_form"]["txtPhotoLink2BodyBuilding"].value;
+        if (pl_bb_2 == "") {
+            alert("please enter photo link 2");
+            return false;
+        }
+
+        // photo link 3
+        let pl_bb_3 = document.forms["Jerai_form"]["txtPhotoLink3BodyBuilding"].value;
+        if (pl_bb_3 == "") {
+            alert("please enter photo link 3");
+            return false;
+        }
+    //}
+
+    // powerlifting tab
+    //else if (tabName === 'TabPowerlifting') {
 
         // weight classes for men
         var men_weight = document.getElementById("selectMenWeight");
@@ -96,36 +130,37 @@ function validate()
             return false;
         }
 
-    } else {
+        // date of completion
+        var dateInput = document.getElementById("txtDatePowerlifting").value;
+        if (dateInput === "") {
+            alert("Please select a date");
+        }
+
+        // photo link 1
+        let pl_pl_1 = document.forms["Jerai_form"]["txtPhotoLink1Powerlifting"].value;
+        if (pl_pl_1 == "") {
+            alert("please enter photo link 1");
+            return false;
+        }
+
+        // photo link 2
+        let pl_pl_2 = document.forms["Jerai_form"]["txtPhotoLink2Powerlifting"].value;
+        if (pl_pl_2 == "") {
+            alert("please enter photo link 2");
+            return false;
+        }
+
+        // photo link 3
+        let pl_pl_3 = document.forms["Jerai_form"]["txtPhotoLink3Powerlifting"].value;
+        if (pl_pl_3 == "") {
+            alert("please enter photo link 3");
+            return false;
+        }
+    //}
+    //else {
         // if not selected either body building or power lifting
-        console.log("please fill information from bodybuilding tab or power lifting tab");
-    }
-  
-    // date of completion
-    let date = document.getElementById("txtdateOfBodyBuilding").value;
-    let dateObj = new Date(date);
-    console.log(date);
-
-    // photo link 1
-    let ph1 = document.forms["Jerai_form"]["txtPhotoLink1BodyBuilding"].value;
-    if (ph1 == "") {
-        alert("please enter photo link 1");
-        return false;
-    }
-
-    // photo link 2
-    let ph2 = document.forms["Jerai_form"]["txtPhotoLink2BodyBuilding"].value;
-    if (ph2 == "") {
-        alert("please enter photo link 2");
-        return false;
-    }
-
-    // photo link 3
-    let ph3 = document.forms["Jerai_form"]["txtPhotoLink3BodyBuilding"].value;
-    if (ph3 == "") {
-        alert("please enter photo link 3");
-        return false;
-    }
+      //  console.log("please fill information from bodybuilding tab or power lifting tab");
+    //}
 
     // terms and conditions checkbox
     var checkBox = document.getElementById("cboxAgree");

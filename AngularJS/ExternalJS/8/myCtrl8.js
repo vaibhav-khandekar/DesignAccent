992c9ce3bff3ffa49381a5acb9f0844acdf39530
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-04-10 18:16:54 +0530

    Create myCtrl8.js

---
+++ AngularJS/ExternalJS/8/myCtrl8.js
@@ -0,0 +1,4 @@
+app.controller("myCtrl8", function ($scope) {
+    $scope.list = [{ name: 'Vaibhav', location: 'Mumbai' }, { name: 'Vaibhavi', location: 'Pune' },
+    { name: 'Vikas', location: 'Delhi' }, { name: 'Vivek', location: 'Hyderabad' }];
+});

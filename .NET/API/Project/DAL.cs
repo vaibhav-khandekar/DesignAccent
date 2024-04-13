7e735ac165d4db39e76d167ce5650ddfb76a9848
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-04-13 17:57:05 +0530

    Update DAL.cs

--- .NET/API/Project/DAL.cs
+++ .NET/API/Project/DAL.cs
@@ -60,5 +60,105 @@ namespace Employees.DAL
             }
             return objEmpDTO;
         }
+        public EmployeesDTO AddEmployeeDetails(string EmpName, string EmpSalary, string EmpAge)
+        {
+            using (command = db.GetStoredProcCommand("[SP_EmpData]"))
+            {
+                db.AddInParameter(command, "@Action", DbType.String, "INSERT");
+                db.AddInParameter(command, "@EmpName", DbType.String, EmpName);
+                db.AddInParameter(command, "@EmpSalary", DbType.String, EmpSalary);
+                db.AddInParameter(command, "@EmpAge", DbType.String, EmpAge);
+            }
+            try
+            {
+                string inserted = (string)db.ExecuteScalar(command);
+
+                if (inserted == "SUCCESS")
+                {
+                    objEmpDTO.Message = "Successful";
+                    objEmpDTO.Code = (int)ErrorCode.ErrorType.SUCCESS;
+                }
+                else if (inserted == "EXISTS")
+                {
+                    objEmpDTO.Message = "Exists";
+                    objEmpDTO.Code = (int)ErrorCode.ErrorType.ERROR;
+                }
+                else
+                {
+                    objEmpDTO.Message = "Unsuccessful";
+                    objEmpDTO.Code = (int)ErrorCode.ErrorType.ERROR;
+                }
+            }
+            catch (Exception ex)
+            {
+                objEmpDTO.Message = "Exception";
+                objEmpDTO.Code = (int)ErrorCode.ErrorType.ERROR;
+                ErrorLog("AddEmployeeDetails", "EmployeesDAL", ex.ToString());
+            }
+            return objEmpDTO;
+        }
+        public EmployeesDTO UpdateEmployeeDetails(int EmpID, string EmpName, string EmpSalary, string EmpAge)
+        {
+            using (command = db.GetStoredProcCommand("[SP_EmpData]"))
+            {
+                db.AddInParameter(command, "@Action", DbType.String, "UPDATE");
+                db.AddInParameter(command, "@EmpID", DbType.String, EmpID);
+                db.AddInParameter(command, "@EmpName", DbType.String, EmpName);
+                db.AddInParameter(command, "@EmpSalary", DbType.String, EmpSalary);
+                db.AddInParameter(command, "@EmpAge", DbType.String, EmpAge);
+            }
+            try
+            {
+                string updated = (string)db.ExecuteScalar(command);
+
+                if (updated == "SUCCESS")
+                {
+                    objEmpDTO.Message = "Successful";
+                    objEmpDTO.Code = (int)ErrorCode.ErrorType.SUCCESS;
+                }
+                else
+                {
+                    objEmpDTO.Message = "Unsuccessful";
+                    objEmpDTO.Code = (int)ErrorCode.ErrorType.ERROR;
+                }
+            }
+            catch (Exception ex)
+            {
+                objEmpDTO.Message = "Exception";
+                objEmpDTO.Code = (int)ErrorCode.ErrorType.ERROR;
+                ErrorLog("UpdateEmployeeDetails", "EmployeesDAL", ex.ToString());
+            }
+            return objEmpDTO;
+        }
+        public EmployeesDTO DeleteEmployeeDetails(int EmpID)
+        {
+            using (command = db.GetStoredProcCommand("[SP_EmpData]"))
+            {
+                db.AddInParameter(command, "@Action", DbType.String, "DELETE");
+                db.AddInParameter(command, "@EmpID", DbType.String, EmpID);
+            }
+            try
+            {
+                string deleted = (string)db.ExecuteScalar(command);
+
+                if (deleted == "SUCCESS")
+                {
+                    objEmpDTO.Message = "Successful";
+                    objEmpDTO.Code = (int)ErrorCode.ErrorType.SUCCESS;
+                }
+                else
+                {
+                    objEmpDTO.Message = "Unsuccessful";
+                    objEmpDTO.Code = (int)ErrorCode.ErrorType.ERROR;
+                }
+            }
+            catch (Exception ex)
+            {
+                objEmpDTO.Message = "Exception";
+                objEmpDTO.Code = (int)ErrorCode.ErrorType.ERROR;
+                ErrorLog("DeleteEmployeeDetails", "EmployeesDAL", ex.ToString());
+            }
+            return objEmpDTO;
+        }
     }
 }

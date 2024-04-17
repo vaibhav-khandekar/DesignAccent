2991c02ce17cc8517e4185713595f781c80a45f6
Vaibhav Khandekar <32119720+vaibhav-khandekar@users.noreply.github.com>
2024-04-17 11:49:50 +0530

    Create ErrorCode.cs

---
+++ .NET/StudentsMGMT-API/DAL/ErrorCode.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsMGMT.DAL
+{
+    public class ErrorCode
+    {
+        public enum ErrorType
+        {
+            NOTEXISTS = 101,
+            SUCCESS = 201,
+            ERROR = 301,
+            LOGINSUCCESS = 401,
+            EXISTS = 501,
+            DATANOTFOUND = 601,
+        }
+    }
+}
